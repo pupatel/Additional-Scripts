2c4bda20e3e4a30a91519b62875ccdd36869bd49
Parth Patel <pupatel@udel.edu>
2018-11-26 23:58:21 -0500

    Update ROCR.R

--- ROCR.R
+++ ROCR.R
@@ -1,43 +1,34 @@
-
-
+# Install the ROCR package
+install.packages('ROCR')
 # loading ROCR library
-library("ROCR")
-# loading active compounds, or compounds with label1
-active <- read.table("sample.active", sep=",", header=FALSE)
-# loading inactive compounds, or compounds with label2
-inactive <- read.table("sample.inactive", sep=",", header=FALSE)
-# binding them and converting to matrix because ROCR works with matrix data
-target_pred <- as.matrix(rbind(active,inactive))
-# because number of the colums should be the same - making additional param
-ncol <- ncol(inactive)
-# generating classes (1 for active, 0 for inactive, but it can be 1 and -1 - there is no difference)
-class.active <- matrix(sample(1, (ncol(active)*nrow(active)), replace=T), ncol=ncol)
-class.inactive <- matrix(sample(0, (ncol(inactive)*nrow(inactive)), replace=T), ncol=ncol)
-# binding the classes
-target_class <- rbind(class.active,class.inactive)
-#target_class1 <- target_class[,1]
+library(ROCR)
+# load data
+data(ROCR.hiv)
+attach(ROCR.hiv)
+
+# ROCR.hiv$hiv.svm contains svm classification data
+pred.svm <- prediction(hiv.svm$predictions, hiv.svm$labels)
 
 # calculating the values for ROC curve,auc,accuracy,specificity,sensitivity
 pred <- prediction(target_pred, target_class)
-perf.1 <- performance(pred,measure="tpr",x.measure="fpr")
-perf.2 <- performance(pred,measure="acc",x.measure="cutoff")
-perf.3 <- performance(pred,measure="auc")
-perf.4 <- performance(pred,measure="spec")
-perf.5 <- performance(pred,measure="sens")
+perf.1 <- performance(pred.svm,measure="tpr",x.measure="fpr")
+perf.2 <- performance(pred.svm,measure="acc",x.measure="cutoff")
+perf.3 <- performance(pred.svm,measure="auc")
+perf.4 <- performance(pred.svm,measure="spec")
+perf.5 <- performance(pred.svm,measure="sens")
 
 # changing params for the ROC plot - width, etc
 par(mar=c(5,5,2,2),xaxs = "i",yaxs = "i",cex.axis=1.3,cex.lab=1.4)
 
 # plotting the ROC curve
-plot(perf.1,col="black",lty=3, lwd=3)
+plot(perf.1,col="red",lty=3, lwd=3)
 
 ## Accuracy plot vs cutoff plot
 # plot(perf.2,lty=3,lwd=0.5,col="lightpink")
 # abline(v=0.2213)
 # plot(perf.2,avg="vertical",col="lightcoral",spread.estimate="boxplot",box.lty=7, box.lwd=5, box.col="slategray4",lwd=3,add=T)
 
-
-# now converting S4 class to vector
+# converting S4 class to vector
 perf.3  <- unlist(slot(perf.3 , "y.values"))
 # adding min and max ROC AUC to the center of the plot
 minauc<-min(round(auc, digits = 2))
